4dfd3fa651d96c6aa1bb28e3c70b0c1b5add0cbd
HaveTime.DrinkTea <havetimedrinktea@gmail.com>
2023-02-03 15:01:18 +0000

    Setup folder structure & gitignore

---
+++ assets/scripts/script.js
@@ -0,0 +1,22 @@
+//-- Whole-script strict mode syntax
+"use strict";
+
+//-- JQuery wrapper
+$(document).ready(function() {
+   //-- START of document.ready
+   //--====================== 
+   //--====================== 
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   //--====================== 
+   //--======================  
+   //-- END of document.ready
+});   
