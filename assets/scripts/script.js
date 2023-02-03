9da9e24257e7fa6299faf20e423d6c4a5eed26a3
HaveTime.DrinkTea <havetimedrinktea@gmail.com>
2023-02-03 16:01:27 +0000

    Set up bootstrap and css

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,7 +6,30 @@ $(document).ready(function() {
    //-- START of document.ready
    //--====================== 
    //--====================== 
+   var APIKey = "23c1d2729442f28b96176ff1560c919f";
+
+   let cityName = "london";
+   queryUrlGetLoc = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey;
+
+   // Here we are building the URL we need to query the database
+   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + APIKey;
    
+
+   $.ajax({
+      url: queryUrlGetLoc,
+      method: "GET"
+      }).then(function(resLocation) {
+         console.log("resLocation:", resLocation);
+      });
+   
+   
+
+   $.ajax({
+      url: queryURL,
+      method: "GET"
+      }).then(function(resWeather) {
+         console.log("resWeather:", resWeather);
+      });
    
    
    
