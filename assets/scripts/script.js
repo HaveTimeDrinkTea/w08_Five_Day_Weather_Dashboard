44bf5069d058f4e197d88ff6aa312ff60d6d089d
HaveTime.DrinkTea <havetimedrinktea@gmail.com>
2023-02-04 14:24:23 +0000

    Added day.js interpretation of unix dates.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -16,6 +16,15 @@ $(document).ready(function() {
       lon : "",
    };
 
+   var todayDate = new Date();
+   var todayDateString = dayjs(todayDate).format("DD-MMM-YYYY");
+
+   var todayDateUnix = Date.parse(todayDateString);
+   console.log("todayDate:", todayDate);
+   console.log("todayDateString:", todayDateString);
+   console.log("todayDateUnix:", todayDateUnix);
+
+
    function getWeather() {
       // Get the current weather data for a location
       let queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + locCorr.lat + "&lon=" + locCorr.lon + "&units=metric&appid=" + APIKey;
@@ -73,17 +82,49 @@ $(document).ready(function() {
       }).fail(function(e) {
          console.log("error is:", e)
          console.log("response:", e.responseJSON);
+         // need to catch the error here!!!!
          return;
-
       });
    }
 
    getLocCorr(cityName);
 
-   console.log("day is:", dayjs.unix(1675522800).format("DD-MMM-YYYY, HH:MM"));
+   console.log("compare date unix 1675663600 day is:", dayjs(1675663600 * 1000).format("DD-MMM-YYYY, HH:MM"));
+   console.log("today unix 1680390000 day is:", dayjs(1675468800000).format("DD-MMM-YYYY, HH:MM"));
+   
+
+   let test = dayjs(1675519348000).isBefore(dayjs(1675663600 * 1000)) ;
+
+   console.log("test:", test, "bec", dayjs(1675519348000).format("DD-MMM-YYYY"), " is before", dayjs(1675663600 * 1000).format("DD-MMM-YYYY"));
+
+   function getDate(unixdate) {
+
+      let dayString = dayjs.unix(unixdate).format("DD-MMM-YYYY, HH:MM");
+      return dayString;
+   }
+
+   function prep5DayData(resLocation) {
+      
+      let dayCounter = 0;
+      let createArrayString ="let day" + dayCounter +";";
+      let createArray;
 
 
+      for (let i = 0; i < resLocation.length; i++) {
+         
+         // if ( === ) {
 
+
+         // } 
+         dayCounter++;
+         createArray = eval(createArrayString);
+
+      };
+      
+   }
+
+
+//--PW temperature, precipitation, pressure, wind, humidity, and cloudiness.
 
 
    //--====================== 
